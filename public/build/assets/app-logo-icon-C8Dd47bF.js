0fef79efacced264e44a0a527d27e9f5b6d14756
Zaki Dzulfikar <berckjack@gmail.com>
2026-05-04 09:29:38 +0700

    chore(deps): update dependencies to latest versions also publicing build

---
+++ public/build/assets/app-logo-icon-C8Dd47bF.js
@@ -0,0 +1,1 @@
+import{c as i,j as c}from"./app-C45CgW1A.js";function s(n){const o=i.c(2);let t;return o[0]!==n?(t=c.jsx("img",{src:"/favicon.png",alt:"Singgah Logo",...n}),o[0]=n,o[1]=t):t=o[1],t}export{s as A};
---
+++ public/build/assets/bookmark-C7I_kUAi.js
@@ -0,0 +1,1 @@
+import{c as o}from"./createLucideIcon-D5SoEG1S.js";const a=[["path",{d:"m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z",key:"1fy3hk"}]],e=o("Bookmark",a);export{e as B};
